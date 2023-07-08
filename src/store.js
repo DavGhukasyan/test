32700ed65d5a9b48b3a7d78d13c38ce6fb5a9da5
DavGhukasyan <eropetrosyan.06@mail.ru>
2023-07-08 20:42:50 +0400

    testing

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import Calcslice from "./Calcslice";
+
+const rootReducer = combineReducers({
+    calc : Calcslice
+})
+
+export const store = configureStore({
+    reducer: rootReducer
+})
+
+
+
